a27f4155a72d51e3392fc9299dbb75846c860947
shohagnawaz <snawazdigital@gmail.com>
2025-03-18 17:36:02 +0600

    tanStack

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
     app.get("/menu", async(req, res) => {
       const result = await menuCollection.find().toArray();
@@ -34,6 +35,17 @@ async function run() {
 
     app.get("/reviews", async(req, res) => {
       const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Carts Collection
+    app.get("/carts", async(req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/carts", async(req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
 
